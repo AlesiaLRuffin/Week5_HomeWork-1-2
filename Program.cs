33267d02ca94da4ca8822e3fec0c18d5657eb95e
Alesia - Imali SuMaku Kute <73201769+AlesiaLRuffin@users.noreply.github.com>
2020-11-27 19:06:01 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessBoard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, remainder;
+            for (i = 1; i <= 8; i++)
+            {
+                if (i % 2 == 0) { remainder = 1; }
+                else { remainder = 0; }
+                for (j = 1; j <= 8; j++)
+                {
+                    if (j % 2 == remainder)
+                    { Console.Write("O"); }
+                    else { Console.Write("X"); }
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
